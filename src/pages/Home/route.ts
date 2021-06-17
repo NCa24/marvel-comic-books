ff40ab50a08ea9f1d8ff968db4bd2847541d1fa7
nelson.cardoso <nelson.mrcardoso@gmail.com>
2021-06-17 15:35:43 +0100

    add basic routing and create first empty page

---
+++ src/pages/Home/route.ts
@@ -0,0 +1,9 @@
+import Home from './index';
+
+const route = {
+	exact: false,
+	path: '/',
+	component: Home,
+}
+
+export default route;
---
+++ src/routes.ts
@@ -0,0 +1,7 @@
+import HomeRoute from "./pages/Home/route";
+
+const routes = [
+	HomeRoute,
+];
+
+export default routes;
