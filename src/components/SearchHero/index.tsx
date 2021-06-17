5a50fe289be8835afb5ea341dab62f387095d57c
nelson.cardoso <nelson.mrcardoso@gmail.com>
2021-06-17 20:47:15 +0100

    create SearchHero component

---
+++ src/components/SearchHero/index.tsx
@@ -0,0 +1,27 @@
+import React, { useCallback, useMemo } from 'react';
+import './index.css';
+import marvelLogo from '../../images/marvel_logo.png';
+import { debounce } from 'lodash';
+
+type SearchHeroProps = {
+	onChange?: (newValue: string) => void;
+}
+
+export default function SearchHero( { onChange }: SearchHeroProps ): React.ReactElement {
+	const onChangeHandler = useCallback((ev: React.ChangeEvent<HTMLInputElement>) => {
+		onChange?.(ev.target.value);
+	}, [onChange]);
+
+	const debouncedChangeHandler = useMemo(
+		() => debounce(onChangeHandler, 500)
+	  , [onChangeHandler]);
+
+	return (
+		<div className="search-hero-container">
+			<div>
+				<img alt="marvel_logo" src={marvelLogo}/>
+			</div>
+			<input className="search-hero__input" placeholder="deadpool" onChange={debouncedChangeHandler}/>
+		</div>
+	);
+}
